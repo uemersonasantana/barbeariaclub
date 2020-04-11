8acea830a242a9547e20b4576e4039c7f9028587
Uemerson Santana <uemerson@icloud.com>
2020-04-11 19:40:51 -0300

    Ajustes de funções relacionadas à biblioteca JWT

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 Route::group([
 
@@ -12,15 +15,28 @@ Route::group([
     Route::post('signup', 'AuthController@signup');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
+    //Route::post('me', 'AuthController@me');
+    Route::get('me', function () {
+        $user = JWTAuth::parseToken()->toUser();
+
+        return response()->json(compact('user'));
+    });
     Route::post('sendPasswordResetLink', 'ResetPasswordController@sendEmail');
     Route::post('resetPassword', 'ChangePasswordController@process');
+
+    Route::post('refreshtoken', function(){
+        try {
+            $token = JWTAuth::parseToken()->refresh();
+            return response()->json(compact('token'));
+        }catch (JWTException $exception){
+            return response()->json(['error' => 'token_invalid'],400);
+        }
+    });
 });
-Route::get('/municipios/{uf_id}', 'MunicipioControlador@index');
 
 Route::middleware(['auth:api'])->group(function () {
-    // Precisa estar autenticado e o e-mail ser gmail para conseguir acessar
     Route::post('/ufs', 'UfControlador@index');
+    Route::get('/municipios/{uf_id}', 'MunicipioControlador@index');
     
     Route::post('/agendamentos/find/', 'AgendamentoControlador@index');
     Route::post('/agendamento/edit/', 'AgendamentoControlador@update');
@@ -39,14 +55,3 @@ Route::middleware(['auth:api'])->group(function () {
     Route::post('/cliente/store/', 'ClienteControlador@store');
     Route::delete('/cliente/{id}', 'ClienteControlador@destroy');
 });
-
-/*
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('/barbeiros', 'BarbeiroControlador@index');
-Route::get('/clientes', 'ClienteControlador@index');
-Route::get('/clientes/{nome}', 'ClienteControlador@buscar');
-Route::get('/agendamentos', 'AgendamentoControlador@index');
-Route::post('/agendamentos/novo/', 'AgendamentoControlador@store');*/
